7b5849318bc1af9656a8990982f41dcd93326bfb
rudratrader <163168824+rudratrader@users.noreply.github.com>
2024-03-12 22:20:35 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,87 @@
+import product1 from './assets/arij.webp';
+import product2 from './assets/camphor.webp';
+import product3 from './assets/cup.webp';
+import product4 from './assets/dhoop.webp';
+import product5 from './assets/freshner.webp';
+import product6 from './assets/handwash.webp';
+import product7 from './assets/havan.webp';
+import product8 from './assets/king.webp';
+import product9 from './assets/sandal.webp';
+import product10 from './assets/sticks.webp';
+import product11 from './assets/vapor.jpg';
+import product12 from './assets/wet.webp';
+
+export const PRODUCTS =[
+    {
+        id:1,
+        productName: "Arij",
+        price: 45,
+        productImage: product1,
+    },
+    {
+        id:8,
+        productName: "3 in 1",
+        price: 70,
+        productImage: product8,
+    },
+    {
+        id:2,
+        productName: "Camphor",
+        price: 280,
+        productImage: product2,
+    },
+    {
+        id:3,
+        productName: "Dhoop Cup",
+        price: 180,
+        productImage: product3,
+    },
+    {
+        id:4,
+        productName: "Dhoop",
+        price: 25,
+        productImage: product4,
+    },
+    {
+        id:5,
+        productName: "Freshner",
+        price: 80,
+        productImage: product5,
+    },
+    {
+        id:6,
+        productName: "Hand Wash",
+        price: 99,
+        productImage: product6,
+    },
+    {
+        id:7,
+        productName: "Havan",
+        price: 200,
+        productImage: product7,
+    },
+    {
+        id:9,
+        productName: "Sandal Aggarbatti",
+        price: 70,
+        productImage: product9,
+    },
+    {
+        id:10,
+        productName: "Dhoop Stick",
+        price: 30,
+        productImage: product10,
+    },
+    {
+        id:11,
+        productName: "Vapourizer",
+        price: 120,
+        productImage: product11,
+    },
+    {
+        id:12,
+        productName: "Wet Dhoop",
+        price: 30,
+        productImage: product12,
+    },
+]
